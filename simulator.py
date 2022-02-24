fa55ae30fee4ead243b0b2edc144ea4a38f93c36
Simas Šakenis <simassakenis@dhcp-10-250-143-128.harvard.edu>
2022-02-24 17:49:28 -0500

    Add skeleton code for the simulator

---
+++ simulator.py
@@ -0,0 +1,84 @@
+# The simulator sits in a loop and iteratively updates the state of
+# the system. This state consists of status indications for all
+# logical nodes. A logical node can be in one of four states:
+# (1) “not scheduled”, (2) “waiting for inputs”, (3) “computing”,
+# (4) “completed”. Each logical node must go through these states
+# in this order (potentially skipping some states). The execution
+# starts with all logical nodes in the “not scheduled” state and
+# ends when all logical nodes are in the “completed” state. At each
+# timestep, the simulator observes the current state of the
+# system and determines the next state.
+
+
+class LogicalNode:
+    def __init__(self, number_of_inputs=None, computation_length=None,
+                 output_size=None, computation_timestamp=None, phys_node=None,
+                 input_q=[], in_neighbors=[], out_neighbors=[],
+                 state='not scheduled'):
+        self.number_of_inputs = number_of_inputs
+        self.computation_length = computation_length
+        self.output_size = output_size
+        self.computation_timestamp = computation_timestamp
+        self.phys_node = phys_node
+        self.input_q = input_q
+        self.in_neighbors = in_neighbors
+        self.out_neighbors = out_neighbors
+        self.state = state
+
+
+class Input:
+    def __init__(self, data_size=None, timestamp=None, source_node=None):
+        self.data_size = data_size
+        self.timestamp = timestamp
+        self.source_node = source_node
+
+
+def scheduler(logical_nodes):
+    #TODO
+
+
+def failure(logical_nodes):
+    #TODO
+
+
+def simulator(logical_nodes):
+    current_timestamp = 0
+    while True:
+        new_logical_to_physical_assignments = scheduler(logical_nodes)
+        for logical_node, physical_node in new_logical_to_physical_assignments:
+            assert logical_node.state == 'not scheduled'
+            logical_node.phys_node = physical_node
+            logical_node.input_q = #TODO (update input_q timestamps)
+            logical_node.state = 'waiting for inputs'
+
+        for logical_node in logical_nodes:
+            if logical_node.state == 'waiting for inputs':
+                if (len(logical_node.input_q) == logical_node.number_of_inputs
+                    and max(logical_node.input_q) <= current_timestamp):
+                    logical_node.state = 'computing'
+                    input_sz = sum([inp.data_size for inp in logical_node.input_q])
+                    logical_node.computation_timestamp =
+                        (current_timestamp
+                         + logical_node.computation_length(input_sz))
+
+            if logical_node.state == 'computing':
+                if logical_node.computation_timestamp == current_timestamp:
+                    #TODO: add outputs to the input queues of out-neighbors
+                    logical_node.state = 'completed'
+
+        if all([logical_node.state == 'completed'
+                for logical_node in logical_nodes]):
+            return current_timestamp
+
+        failed_nodes = failure(logical_nodes)
+        for logical_node in failed_nodes:
+            assert logical_node.state in ['waiting for inputs', 'computing']
+            #TODO: (potentially) reset some fields of logical_node
+            logical_node.state = 'not scheduled'
+
+        current_timestamp += 1
+ 
+
+if __name__ == '__main__':
+    logical_nodes = #TODO (create logical graph)
+    execution_time = simulator(logical_nodes)
