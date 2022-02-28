420c606370cb4bee2b50207d94a4ebbe9d2cc43d
Priyan Vaithilingam <priyanmuthu@gmail.com>
2022-02-27 20:23:33 -0500

    minor test changes

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,5 @@
 import unittest
-from simulator.simulator import LogicalNode
+from simulator.simulator import LogicalNode, PhysicalNode
 import random
 
 class MapReduceTest(unittest.TestCase):
@@ -15,21 +15,67 @@ class MapReduceTest(unittest.TestCase):
         reduceComputationLength = 3
 
         # initializing Map Nodes
-        map_nodes = self.create_map_nodes(numMapNodes, (mapComputationLength, mapComputationLength+1), (1,2))
+        map_nodes = HelperFunctions.create_map_nodes(numMapNodes, (mapComputationLength, mapComputationLength+1), (1,2))
         
         # initializing Reduce Nodes
-        reduce_nodes = self.create_reduce_nodes(numReduceNodes, (reduceComputationLength, reduceComputationLength+1), (1,2), map_nodes)
+        reduce_nodes = HelperFunctions.create_reduce_nodes(numReduceNodes, (reduceComputationLength, reduceComputationLength+1), (1,2), map_nodes)
 
         # connecting Reduce Nodes to Map Nodes
-        map_nodes, reduce_nodes = self.connect_reduce_nodes_to_map_nodes(map_nodes, reduce_nodes, 1)
+        map_nodes, reduce_nodes = HelperFunctions.connect_reduce_nodes_to_map_nodes(map_nodes, reduce_nodes, 1)
 
-        for m in map_nodes:
-            print(len(m.out_neighbors))
+        # add physical nodes
+        physical_nodes = HelperFunctions.create_physical_nodes(3, (1,2), (1,2))
 
-        for r in reduce_nodes:
-            print(len(r.in_neighbors))
+    
 
-    def create_map_nodes(self, num_map_nodes, map_computation_range, map_output_range):
+
+class HelperFunctions:
+
+    @staticmethod
+    def create_physical_node(compute_power_range, memory_range, bandwidth_range):
+        '''
+            Function to create physical node
+            compute_power_range: range of compute power for physical nodes
+            memory_range: range of memory for physical nodes
+            bandwidth_range: range of bandwidth for physical nodes
+        '''
+        physical_node = PhysicalNode(
+            compute_power=random.randrange(compute_power_range[0], compute_power_range[1]),
+            memory=random.randrange(memory_range[0], memory_range[1]),
+            bandwidth=random.randrange(bandwidth_range[0], bandwidth_range[1]))
+        
+        return physical_node
+    
+    @staticmethod
+    def create_physical_nodes(num_physical_nodes, compute_power_range, memory_range, bandwidth_range):
+        '''
+            Function to create physical node
+            num_physical_nodes: number of physical nodes to create
+            compute_power_range: range of compute power for physical nodes
+            memory_range: range of memory for physical nodes
+            bandwidth_range: range of bandwidth for physical nodes
+        '''
+        physical_nodes = []
+        for i in range(num_physical_nodes):
+            physical_node = HelperFunctions.create_physical_node(compute_power_range, memory_range, bandwidth_range)
+            physical_nodes.append(physical_node)
+        return physical_nodes
+
+    @staticmethod
+    def create_map_node(map_computation_range, map_output_range):
+        '''
+            Function to create map nodes randomly
+            map_computation_range: range of computation length for map nodes
+            map_output_range: range of output size for map nodes
+        '''
+        logical_map_node = LogicalNode(
+            number_of_inputs=0, 
+            computation_length=random.randrange(map_computation_range[0],map_computation_range[1]), 
+            output_size=random.randrange(map_output_range[0],map_output_range[1]))
+        return logical_map_node
+
+    @staticmethod
+    def create_map_nodes(num_map_nodes, map_computation_range, map_output_range):
         '''
             Function to create map nodes randomly
             num_map_nodes: number of map nodes to create
@@ -38,14 +84,25 @@ class MapReduceTest(unittest.TestCase):
         '''
         map_nodes = []
         for i in range(num_map_nodes):
-            logical_map_node = LogicalNode(
-                number_of_inputs=0, 
-                computation_length=random.randrange(map_computation_range[0],map_computation_range[1]), 
-                output_size=random.randrange(map_output_range[0],map_output_range[1]))
+            logical_map_node = HelperFunctions.create_map_node(map_computation_range, map_output_range)
             map_nodes.append(logical_map_node)
         return map_nodes
     
-    def create_reduce_nodes(self, num_reduce_nodes, reduce_computation_range, reduce_output_range, map_nodes):
+    @staticmethod
+    def create_reduce_node(reduce_computation_range, reduce_output_range, map_nodes):
+        '''
+            Function to create reduce nodes randomly
+            reduce_computation_range: range of computation length for reduce nodes
+            reduce_output_range: range of output size for reduce nodes
+            map_nodes: list of map nodes
+        '''
+        logical_reduce_node = LogicalNode(
+            computation_length=random.randrange(reduce_computation_range[0],reduce_computation_range[1]), 
+            output_size=random.randrange(reduce_output_range[0],reduce_output_range[1]))
+        return logical_reduce_node
+    
+    @staticmethod
+    def create_reduce_nodes(num_reduce_nodes, reduce_computation_range, reduce_output_range, map_nodes):
         '''
             Function to create reduce nodes randomly
             num_reduce_nodes: number of reduce nodes to create
@@ -55,13 +112,12 @@ class MapReduceTest(unittest.TestCase):
         '''
         reduce_nodes = []
         for i in range(num_reduce_nodes):
-            logical_reduce_node = LogicalNode(
-                computation_length=random.randrange(reduce_computation_range[0],reduce_computation_range[1]), 
-                output_size=random.randrange(reduce_output_range[0],reduce_output_range[1]))
+            logical_reduce_node = HelperFunctions.create_reduce_node(reduce_computation_range, reduce_output_range, map_nodes)
             reduce_nodes.append(logical_reduce_node)
         return reduce_nodes
 
-    def connect_reduce_nodes_to_map_nodes(self, map_nodes, reduce_nodes, connection_ratio=0.5):
+    @staticmethod
+    def connect_reduce_nodes_to_map_nodes(map_nodes, reduce_nodes, connection_ratio=0.5):
         '''
             Function to connect reduce nodes to map nodes
             map_nodes: list of map nodes
@@ -78,7 +134,6 @@ class MapReduceTest(unittest.TestCase):
                 mNode.out_neighbors.append(rNode)
                 # should we modify output size of map node?
         return map_nodes, reduce_nodes
-
 
 if __name__ == '__main__':
     unittest.main()
